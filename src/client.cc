efc9a382ce2048736d3e400e9d27141fcd84dd3d
Feng Xiao <xfxyjwf@gmail.com>
2018-03-06 12:42:26 -0800

    udp test

--- src/client.cc
+++ src/client.cc
@@ -2,31 +2,15 @@
 #include <iostream>
 
 using namespace std;
-using asio::ip::tcp;
+using asio::ip::udp;
 
 int main(int argc, char* argv[]) {
   asio::io_service service;
 
-  tcp::resolver resolver(service);
-  tcp::resolver::query query(argv[1], argv[2]);
-  tcp::socket socket(service);
-  asio::connect(socket, resolver.resolve(query));
-
-  cout << "Command: " << flush;
-  string cmd;
-  while (cin >> cmd) {
-    if (cmd == "send") {
-      socket.shutdown(tcp::socket::shutdown_send);
-    } else if (cmd == "receive") {
-      socket.shutdown(tcp::socket::shutdown_receive);
-    } else if (cmd == "both") {
-      socket.shutdown(tcp::socket::shutdown_both);
-    } else if (cmd == "close") {
-      socket.close();
-    } else if (cmd == "exit") {
-      break;
-    } else {
-      cerr << "Unknown command: " << cmd << endl;
-    }
-  }
+  udp::resolver resolver(service);
+  udp::resolver::query query(udp::v4(), argv[1], argv[2]);
+  auto it = resolver.resolve(query);
+  udp::socket socket(service);
+  socket.open(udp::v4());
+  socket.send_to(asio::buffer(string(stoi(argv[3]), 'x')), *it);
 }
--- src/server.cc
+++ src/server.cc
@@ -1,8 +1,32 @@
 #include <asio.hpp>
 #include <iostream>
+#include <string>
+#include <memory>
 
 using namespace std;
+using asio::ip::udp;
+
+char buffer[10 * 1024];
+
+string ToString(const udp::endpoint& endpoint) {
+  return endpoint.address().to_string() + ":" + to_string(endpoint.port());
+}
+
+void StartReceive(udp::socket* socket) {
+  auto remote = make_shared<udp::endpoint>();
+  socket->async_receive_from(asio::buffer(buffer), *remote, [socket, remote](const asio::error_code& ec, size_t len) {
+    if (ec) {
+      cerr << ec.message() << endl;
+      return;
+    }
+    cout << "Received " << len << " bytes " << ToString(*remote) << endl;
+    StartReceive(socket);
+  });
+}
 
 int main() {
   asio::io_service service;
+  udp::socket socket(service, udp::endpoint(udp::v4(), 11234));
+  StartReceive(&socket);
+  service.run();
 }
